51087f9b0053d5f70492341a167da1943ad71b18
kelseybcrow <kelseybcrow@gmail.com>
2022-07-08 08:34:43 -0600

    add login page

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css';
+import '../styles/globals.css'
+import dynamic from 'next/dynamic'
+import '@solana/wallet-adapter-react-ui/styles.css'
+import WalletConnectionProvider from './../context/WalletConnectionProvider'
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    const WalletConnectionProvider = dynamic(
+        () => import('../context/WalletConnectionProvider'),
+        { ssr: false }
+    )
+    return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
