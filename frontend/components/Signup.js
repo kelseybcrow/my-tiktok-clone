459ba8b7e277f8bc1abef6cbcdbc98a3452d7ae6
kelseybcrow <kelseybcrow@gmail.com>
2022-07-08 18:51:04 -0600

    add anchor and solana stuff

--- frontend/components/Signup.js
+++ frontend/components/Signup.js
@@ -14,14 +14,14 @@ const Signup = () => {
             <h1 className={styles.title}>Sign up to use TikTok</h1>
             <div className={styles.signupForm}>
                 <div className={styles.inputField}>
-                    <div className={styles.inputTitle}>Username</div>
+                    <div className={styles.inputTitle}>Username:</div>
                     <div className={styles.inputContainer}>
                         <input className={styles.input} type='text' />
                     </div>
                 </div>
 
                 <div className={styles.inputField}>
-                    <div className={styles.inputTitle}>Password</div>
+                    <div className={styles.inputTitle}>Profile image:</div>
                     <div className={styles.inputContainer}>
                         <input type='text' className={styles.input} />
                     </div>
