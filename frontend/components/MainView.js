e596de0c7ca5009e3cb515554216926d17594fd9
kelseybcrow <kelseybcrow@gmail.com>
2022-07-12 17:11:38 -0600

    create useTiktok hook

--- frontend/components/MainView.js
+++ frontend/components/MainView.js
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react'
 import styles from '../styles/MainView.module.css'
 import Signup from './Signup'
+import UploadModal from './UploadModal'
+import Video from './Video'
+import BottomBar from './BottomBar'
 import { useEffect, useState } from 'react'
 import { useWallet } from '@solana/wallet-adapter-react'
 import { SOLANA_HOST } from './../utils/const'
@@ -8,6 +11,7 @@ import { getProgramInstance } from '../utils/utils'
 import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
 
 import useAccount from '../hooks/useAccount'
+import useTiktok from '../hooks/useTiktok'
 
 const anchor = require('@project-serum/anchor')
 const utf8 = anchor.utils.bytes.utf8
@@ -27,12 +31,76 @@ const MainView = () => {
 
     const program = getProgramInstance(connection, wallet)
 
+    const [tiktoks, setTikToks] = useState()
+
+    const [newVideoShow, setNewVideoShow] = useState(false)
+    const [description, setDescription] = useState('')
+    const [videoUrl, setVideoUrl] = useState('')
+    const [userDetail, setUserDetail] = useState()
+
     const { signup } = useAccount()
+    const { getTiktoks, likeVideo, createComment, newVideo, getComments } =
+        useTiktok(
+            setTikToks,
+            userDetail,
+            videoUrl,
+            description,
+            setDescription,
+            setVideoUrl,
+            setNewVideoShow
+        )
+
+    useEffect(() => {
+        if (wallet.connected) {
+            checkAccount()
+            getTiktoks()
+        }
+    }, [])
+
+    const checkAccount = async () => {
+        let [user_pda] = await anchor.web3.PublicKey.findProgramAddress(
+            [utf8.encode('user'), wallet.publicKey.toBuffer()],
+            program.programId
+        )
+
+        try {
+            const userInfo = await program.account.useAccount.fetch(userInfo)
+            console.log(userInfo)
+            setUserDetail(userInfo)
+            setAccount(true)
+        } catch (error) {
+            setAccount(false)
+        }
+    }
 
     return (
         <>
             {isAccount ? (
-                <div>Tiktoks will go here</div>
+                <div>
+                    {newVideoShow && <UploadModal />}
+                    <div className={styles.appVideos}>
+                        {tiktoks.length === 0 ? (
+                            <h1>No videos</h1>
+                        ) : (
+                            tiktoks.map((tiktok, id) => (
+                                <Video
+                                    key={id}
+                                    address={tiktok.publicKey.toBase58()}
+                                    url={tiktok.account.videoUrl}
+                                    channel={tiktok.account.creatorName}
+                                    index={tiktok.account.likes}
+                                    description={tiktok.account.description}
+                                    likeAddress={tiktok.account.peopleWhoLiked}
+                                    commentCount={tiktok.account.commentCount.toNumber()}
+                                    likeVideo={likeVideo}
+                                    createComment={createComment}
+                                    getComments={getComments}
+                                />
+                            ))
+                        )}
+                    </div>
+                    <BottomBar />
+                </div>
             ) : (
                 <Signup signup={signup} wallet={wallet.publicKey.toBase58()} />
             )}
---
+++ frontend/components/Video.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useState } from 'react'
+import styles from '../styles/Video.modules.css'
+
+const Video = (
+    key,
+    address,
+    url,
+    channel,
+    index,
+    description,
+    likeAddress,
+    commentCount,
+    likeVideo,
+    createComment,
+    getComments
+) => {
+    const [playing, setPlaying] = useState(false)
+
+    return <div>Video</div>
+}
+
+export default Video
